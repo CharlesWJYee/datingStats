338f3e07c54cc392da01efdef38f8ef1ece95671
CharlesWJYee <40860731bol947>
2023-02-01 16:16:48 -0600

    preliminary csv read and data structure for key'ing in for income calculation

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import readCSV
 
+def namestr(obj, namespace):
+    return [name for name in namespace if namespace[name] is obj]
+
+
+def displayDicts(dictDict):
+    for d in dictDict:
+        print(d)
+        print(dictDict[d])
+
 
 if __name__ == '__main__':
-    print(readCSV.readCSVtoDict("looks"))
+    displayDicts(readCSV.readCSVtoDict("looks"))
     print("Program finished")
 
--- readCSV.py
+++ readCSV.py
@@ -5,25 +5,28 @@ def readCSVtoDict(csvFileName):
     """
     The input CSV fie column names are:
     attractiveness metric (looks, height, ethinicity), gender, gender (or sometimes ethinicity)
-    And the contents are income in $1000
-    We will read this template file and return a dictionary for
-    :param csvFileName:
-    :return:
+    And the contents are income in $1000 multiples
+    We will read this template file and return a dictionary of dictionaries
+    First layer of dictionary indicates which column of gender (or ethinicity). Second layer of dictionary
+    contains key of metric, and value in income $ values
+    :param csvFileName: input csv file
+    :return: dictionary of dictionaries
     """
     dirname = os.path.dirname(__file__)
     csvFilePath = os.path.join(dirname, "Data")
-    csvFilePath = os.path.join(csvFilePath, csvFileName+".csv")
+    csvFilePath = os.path.join(csvFilePath, csvFileName + ".csv")
 
     readFile = pd.read_csv(csvFilePath)
-    readFile.fillna(0, inplace=True)  # unavailable information "-" and "Not Feasible" are empty in csv, and filled with 0
+    readFile.fillna(0,
+                    inplace=True)  # unavailable information "-" and "Not Feasible" are empty in csv, and filled with 0
 
     fileColumns = list(readFile.columns)[1:]  # ignore first column as it's a type of index
     indexColumn = list(readFile.columns)[0]
-    dictList = [] # return list with dictionaries (male/female, or different ethinicities)
+    dictDict = dict()  # return dict of dictionaries (male/female, or different ethinicities as first layer keys)
 
     for d in fileColumns:
-        locals()[d] = dict()
+        newDict = dict()
         for i, row in readFile.iterrows():
-            locals()[d][row[indexColumn]] = row[d] * 1000  # income per year is always scaled by $1000
-        dictList.append(locals()[d])
-    return dictList
+            newDict[row[indexColumn]] = row[d] * 1000  # income per year is always scaled by $1000
+        dictDict[d] = newDict
+    return dictDict
